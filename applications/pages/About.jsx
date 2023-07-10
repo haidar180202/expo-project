848ccfbd87878972108f6cb7980435a7650aa207
haidar180202 <gshaidar6@gmail.com>
2023-07-10 22:23:34 +0700

    replace simply for about and interest

--- applications/pages/About.jsx
+++ applications/pages/About.jsx
@@ -1,19 +1,29 @@
-import { StyleSheet, Text, View,Image } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
 
 const About = () => {
   return (
-    <View style={{display:"flex", flexDirection:"row", height:120,backgroundColor:'yellow',marginBottom:30,borderRadius:8,justifyContent:"space-between"}}>
-      <Text>About</Text>
-      <Image
-				style={{width:17,height:17,marginBottom:90,marginRight:8}}
-				source={require('../components/assets/editlogo.png')}
-		/>
-    
+    <View style={{        
+        height: "auto",
+        backgroundColor: "yellow",
+        marginBottom: 30,
+        borderRadius: 8
+        }}>
+      <View style={{display:"flex",flexDirection:"row",justifyContent:"space-between"}}>
+        <Text>About</Text>
+        <Image
+          style={{ width: 17, height: 17, marginBottom: 40, marginRight: 8 }}
+          source={require("../components/assets/editlogo.png")}
+        />
+      </View>
+
+      <View>
+      <Text style={{marginBottom:50,textAlign:"center"}}>Add in your your to help others know you better</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default About
+export default About;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- applications/pages/Interest.jsx
+++ applications/pages/Interest.jsx
@@ -3,13 +3,23 @@ import React from 'react'
 
 export default function Interest() {
   return (
-    <View style={{display:"flex", flexDirection:"row", height:120,backgroundColor:'green',marginBottom:30,borderRadius:8,justifyContent:"space-between"}}>
-      <Text>Interest</Text>
-      <Image
-				style={{width:17,height:17,marginBottom:90,marginRight:8}}
-				source={require('../components/assets/editlogo.png')}
-		/>
-    
+    <View style={{        
+        height: 120,
+        backgroundColor: "green",
+        marginBottom: 30,
+        borderRadius: 8
+        }}>
+      <View style={{display:"flex",flexDirection:"row",justifyContent:"space-between"}}>
+        <Text>Interest</Text>
+        <Image
+          style={{ width: 17, height: 17, marginBottom: 40, marginRight: 8 }}
+          source={require("../components/assets/editlogo.png")}
+        />
+      </View>
+
+      <View>
+      <Text style={{marginBottom:50,textAlign:'center'}}>Add in your your to help others know you better</Text>
+      </View>
     </View>
   )
 }
